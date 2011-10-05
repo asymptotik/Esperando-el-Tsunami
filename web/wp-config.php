b4ca6927b450ca5aba518ad56e306a2b8ed8da4c
Rick Boykin <rick@mondorobot.com>
2011-10-04 21:37:01 -0600

    new database

--- web/wp-config.php
+++ web/wp-config.php
@@ -16,16 +16,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'lulacruza');
+define('DB_NAME', 'communik_esperando');
 
 /** MySQL database username */
-define('DB_USER', 'rick');
+define('DB_USER', 'communik_lulacru');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'TheRob0t');
+define('DB_PASSWORD', '#sperand0');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', '184.154.160.8');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
