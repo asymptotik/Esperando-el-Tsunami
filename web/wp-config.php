d13f96951740b3f8c8abd77819624cf270b47839
Rick Boykin <rick@mondorobot.com>
2011-12-01 10:43:41 -0700

    Added facebook images

--- web/wp-config.php
+++ web/wp-config.php
@@ -19,13 +19,13 @@
 define('DB_NAME', 'lulacruza');
 
 /** MySQL database username */
-define('DB_USER', 'rick');
+define('DB_USER', '');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'TheRob0t');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', '');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
